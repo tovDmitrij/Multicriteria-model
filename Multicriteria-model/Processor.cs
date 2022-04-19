ae9b086f489dc12ed4cae64ec35f3533441c97fe
D X <DMXikka@gmail.com>
2022-04-20 01:38:04 +0300

    Возможно удачная попытка переезда на .Net 6

---
+++ Multicriteria-model/Processor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multicriteria_model
+{
+    class Processor: Product, ICores, IFrequency
+    {
+        private readonly string name;
+        private readonly uint cores;
+        private readonly double frequency;
+        private readonly uint price;
+        public override string Name => name;
+        public uint Cores => cores;
+        public double Frequency => frequency;
+        public override uint Price => price;
+        public Processor(string name, uint cores, double frequency, uint price)
+        {
+            this.name = name;
+            this.cores = cores;
+            this.frequency = frequency;
+            this.price = price;
+        }
+    }
+}
---
+++ Multicriteria-model/Videocard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multicriteria_model
+{
+    class Videocard: Product, IMemory, IFrequency
+    {
+        private readonly string name;
+        private readonly uint videoMemory;
+        private readonly uint frequency;
+        private readonly uint price;
+        public override string Name => name;
+        public uint Memory => videoMemory;
+        public double Frequency => frequency;
+        public override uint Price => price;
+        public Videocard(string name, uint videoMemory, uint frequency, uint price)
+        {
+            this.name = name;
+            this.videoMemory = videoMemory;
+            this.frequency = frequency;
+            this.price = price;
+        }
+    }
+}
