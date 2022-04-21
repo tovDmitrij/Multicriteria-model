124e916dc24d84a37bd127a0b7f672e4ae582722
TovDmitrij <dmxikka@gmail.com>
2022-04-21 22:48:27 +0300

    В стадии разработки Обобщённый критерий

--- Multicriteria-model/GeneralizedCriterion.cs
+++ Multicriteria-model/GeneralizedCriterion.cs
@@ -12,13 +12,60 @@ namespace Multicriteria_model
     class GeneralizedCriterion<T> where T : Product
     {
         private readonly List<T> products;
-        public GeneralizedCriterion(List<T> products)
+        private readonly SortedDictionary<Characteristics, double> weights;
+        public GeneralizedCriterion(List<T> products, SortedDictionary<Characteristics, double> weights)
         {
             this.products = products;
+            this.weights = weights;
         }
         public List<T> Run()
         {
-            return products;
+            double[] summ = genCriterion();
+            List<T> newList = products;
+            for (int i = 0; i < summ.Length; i++)
+                if (summ[i] == summ.Max())
+                    newList.Add(products[i]);
+            return newList;
+        }
+        private double[] genCriterion()
+        {
+            double[,] tableSumm = new double[products.Count, weights.Count];
+            double[] summ = new double[products.Count];
+            try
+            {
+                switch (products)
+                {
+                    #region Жесткие диски
+                    case List<HDD>:
+                        List<HDD> HDDList = products as List<HDD>;
+                        for(int i = 0; i < products.Count; i++)
+                        {
+                            for (int j = 0; j < weights.Count; j++)
+                            {
+                                tableSumm[i,j] = HDDList[i].Memory
+                                //switch (weights.ElementAt(j).Key)
+                                //{
+                                //    case Characteristics.Price:
+                                //        tableSumm[i, j] = HDDList[i].Price * weights.ElementAt(j).Value;
+                                //        break;
+                                //    case Characteristics.Memory:
+                                //        tableSumm[i, j] = HDDList[i].Memory * weights.ElementAt(j).Value;
+                                //        break;
+                                //    case Characteristics.Speed:
+                                //        tableSumm[i,j]
+                                //}
+                            }
+                        }
+                        break;
+                    #endregion
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}");
+                return null;
+            }
+            return summ;
         }
     }
 }
--- Multicriteria-model/ParetoOptimum.cs
+++ Multicriteria-model/ParetoOptimum.cs
@@ -22,76 +22,85 @@ namespace Multicriteria_model
         }
         private int[]? ParetoArray()
         {
-            int[,] paretoArray = new int[products.Count, products.Count];
-            int[] summ = new int[products.Count];
-            switch (products)
+            try
             {
-                #region Жесткие диски
-                case List<HDD>:
-                    List<HDD> HDDList = products as List<HDD>();
-                    for (int i = 0; i < products.Count; i++)
-                        for (int j = 0; j < products.Count; j++)
-                            paretoArray[i, j] = HDDList[j].Price < HDDList[i].Price || 
-                                HDDList[j].Speed > HDDList[i].Speed || 
-                                HDDList[j].Memory > HDDList[i].Memory ? 1 : 0;
-                    break;
-                #endregion
-
-                #region Оперативная память
-                case List<RAM>:
-                    List<RAM> RAMList = products as List<RAM>();
-                    for (int i = 0; i < products.Count; i++)
-                        for (int j = 0; j < products.Count; j++)
-                            paretoArray[i, j] = RAMList[j].Price < RAMList[i].Price || 
-                                RAMList[j].Frequency > RAMList[i].Frequency || 
-                                RAMList[j].Memory > RAMList[i].Memory ? 1 : 0;
-                    break;
-                #endregion
-
-                #region Видеокарты
-                case List<Videocard>:
-                    List<Videocard> VideocardList = products as List<Videocard>();
-                    for (int i = 0; i < products.Count; i++)
-                        for (int j = 0; j < products.Count; j++)
-                            paretoArray[i, j] = VideocardList[j].Price < VideocardList[i].Price || 
-                                VideocardList[j].Frequency > VideocardList[i].Frequency || 
-                                VideocardList[j].Memory > VideocardList[i].Memory ? 1 : 0;
-                    break;
-                #endregion
-
-                #region Процессоры
-                case List<Processor>:
-                    List<Processor> ProcessorList = products as List<Processor>();
-                    for (int i = 0; i < products.Count; i++)
-                        for (int j = 0; j < products.Count; j++)
-                            paretoArray[i, j] = ProcessorList[j].Price < ProcessorList[i].Price ||
-                                ProcessorList[j].Frequency > ProcessorList[i].Frequency ||
-                                ProcessorList[j].Cores > ProcessorList[i].Cores ? 1 : 0;
-                    break;
-                #endregion
-
-                #region Мониторы
-                case List<Monitor>:
-                    List<Monitor> MonitorList = products as List<Monitor>();
-                    for (int i = 0; i < products.Count; i++)
-                        for (int j = 0; j < products.Count; j++)
-                            paretoArray[i, j] = MonitorList[j].Price < MonitorList[i].Price ||
-                                MonitorList[j].Frequency > MonitorList[i].Frequency ||
-                                MonitorList[j].ScreenSize > MonitorList[i].ScreenSize ? 1 : 0;
-                    break;
-                #endregion
-
-                default:
-                    return null;
-            }
-            for (int i = 0; i < products.Count; i++)
-            {
-                for(int j = 0; j <= products.Count; j++)
+                int[,] paretoArray = new int[products.Count, products.Count];
+                int[] summ = new int[products.Count];
+                switch (products)
                 {
-                    summ[i] += paretoArray[i, j];
+                    #region Жесткие диски
+                    case List<HDD>:
+                        List<HDD> HDDList = products as List<HDD>;
+                        for (int i = 0; i < products.Count; i++)
+                            for (int j = 0; j < products.Count; j++)
+                                paretoArray[i, j] = HDDList[j].Price < HDDList[i].Price || 
+                                    HDDList[j].Speed > HDDList[i].Speed || 
+                                    HDDList[j].Memory > HDDList[i].Memory ? 1 : 0;
+                        break;
+                    #endregion
+
+                    #region Оперативная память
+                    case List<RAM>:
+                        List<RAM> RAMList = products as List<RAM>;
+                        for (int i = 0; i < products.Count; i++)
+                            for (int j = 0; j < products.Count; j++)
+                                paretoArray[i, j] = RAMList[j].Price < RAMList[i].Price || 
+                                    RAMList[j].Frequency > RAMList[i].Frequency || 
+                                    RAMList[j].Memory > RAMList[i].Memory ? 1 : 0;
+                        break;
+                    #endregion
+
+                    #region Видеокарты
+                    case List<Videocard>:
+                        List<Videocard> VideocardList = products as List<Videocard>;
+                        for (int i = 0; i < products.Count; i++)
+                            for (int j = 0; j < products.Count; j++)
+                                paretoArray[i, j] = VideocardList[j].Price < VideocardList[i].Price || 
+                                    VideocardList[j].Frequency > VideocardList[i].Frequency || 
+                                    VideocardList[j].Memory > VideocardList[i].Memory ? 1 : 0;
+                        break;
+                    #endregion
+
+                    #region Процессоры
+                    case List<Processor>:
+                        List<Processor> ProcessorList = products as List<Processor>;
+                        for (int i = 0; i < products.Count; i++)
+                            for (int j = 0; j < products.Count; j++)
+                                paretoArray[i, j] = ProcessorList[j].Price < ProcessorList[i].Price ||
+                                    ProcessorList[j].Frequency > ProcessorList[i].Frequency ||
+                                    ProcessorList[j].Cores > ProcessorList[i].Cores ? 1 : 0;
+                        break;
+                    #endregion
+
+                    #region Мониторы
+                    case List<Monitor>:
+                        List<Monitor> MonitorList = products as List<Monitor>;
+                        for (int i = 0; i < products.Count; i++)
+                            for (int j = 0; j < products.Count; j++)
+                                paretoArray[i, j] = MonitorList[j].Price < MonitorList[i].Price ||
+                                    MonitorList[j].Frequency > MonitorList[i].Frequency ||
+                                    MonitorList[j].ScreenSize > MonitorList[i].ScreenSize ? 1 : 0;
+                        break;
+                    #endregion
+
+                    default:
+                        return null;
                 }
+                for (int i = 0; i < products.Count; i++)
+                {
+                    for(int j = 0; j <= products.Count; j++)
+                    {
+                        summ[i] += paretoArray[i, j];
+                    }
+                }
+                return summ;
+
             }
-            return summ;
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}");
+                return null;
+            }
         }
     }
 }
